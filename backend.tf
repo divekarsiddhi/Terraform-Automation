75c413db1997880b1fe6a6b6e181644eb3081e9c
divekarsiddhi <90193386+divekarsiddhi@users.noreply.github.com>
2026-04-19 16:05:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "sample-terraform-dev-project-batch-31"
     key = "main"
     region = "us-east-1"
-   dynamodb_table = "my-dynamodb-table"
+  # dynamodb_table = "my-dynamodb-table"
   }
 }
